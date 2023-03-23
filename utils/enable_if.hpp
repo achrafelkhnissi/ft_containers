0813799fd9fe2620e5b9709942d9ac6fa7380b45
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-23 15:14:54 +0000

    add: enable_if

---
+++ utils/enable_if.hpp
@@ -0,0 +1,14 @@
+#ifndef ENABLE_IF_HPP
+#define ENABLE_IF_HPP
+
+namespace ft {
+	template <bool B, class T = void>
+	struct enable_if {};
+
+	template <class T>
+	struct enable_if<true, T> {
+		typedef T type;
+	};
+}
+
+#endif /* ENABLE_IF_HPP */
