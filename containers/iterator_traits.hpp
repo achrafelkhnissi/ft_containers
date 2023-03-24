ea813b488f5aff7f47a7ede9e51096b9ce9271e7
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-24 00:59:41 +0000

    add: iterator traits

--- containers/iterator_traits.hpp
+++ containers/iterator_traits.hpp
@@ -7,7 +7,7 @@ namespace ft {
 
     template<class Iterator>
     struct iterator_traits {
-        typedef typename Iterator::std::ptrdiff_t       difference_type;
+        typedef typename Iterator::difference_type       difference_type;
         typedef typename Iterator::value_type           value_type;
         typedef typename Iterator::pointer              pointer;
         typedef typename Iterator::reference            reference;
